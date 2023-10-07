4839e36ad4763aaf6b2e8e85b23644dd03636561
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-10-07 02:53:31 -0700

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i + '0');
+
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+
+	putchar('\n');
+
+	return (0);
+}

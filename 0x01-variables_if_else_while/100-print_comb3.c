18fc7099aafa65ccd8608b97b01cb509819245b9
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-10-07 10:32:24 -0700

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-	int num1 = 0;
-	int num2 = 0;
+	int num1 = '0';
+	int num2 = '0';
 
-	while (num1 <= 9)
+	while (num1 <= '9')
 	{
-		while (num2 <= 9)
+		while (num2 <= '9')
 		{
 			if (num1 < num2)
 			{
 				putchar(num1);
 				putchar(num2);
-				if (num1 != 8 || (num1 == 8 && num2 != 9))
+				if (num1 != '8' || (num1 == '8' && num2 != '9'))
 				{
 					putchar(',');
 					putchar(' ');

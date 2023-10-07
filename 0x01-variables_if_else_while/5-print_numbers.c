7d6a6b81a1b9fc4908a53e45c4cc0a21bf8edf24
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-10-07 02:16:04 -0700

    Figure

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: Always 0, program end point
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+
+}

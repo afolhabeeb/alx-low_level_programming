4aca95327397a240dc05004f254c26c364172298
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-10-11 17:01:14 -0700

    Print Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ */
+
+
+void print_alphabet(void)
+{
+
+	char al;
+
+	al = 'a';
+
+	while (al <= 'z')
+	{
+		_putchar(al);
+		al++;
+	}
+
+		_putchar('\n');
+
+	}
---
+++ 0x02-functions_nested_loops/samp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ *
+ * Return: (0)
+ *
+ */
+
+int main(void)
+{
+
+	char c[] = "_putchar";
+
+	_putchar(c[0, 1, 2, 3, 4, 5, 6, 7]);
+
+	return (0);
+
+
+
+}

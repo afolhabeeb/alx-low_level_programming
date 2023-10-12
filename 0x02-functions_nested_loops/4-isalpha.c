7d06d0ac0a1e1882699013f3c7ec8d4db6f72ab7
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-10-11 18:44:15 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabetic character
+ *
+ * Description: _putchar is used to prints
+ * @c: collect alphabet
+ *
+ * Return: (1) if c is a letter, lowercase or uppercae (0) if otherwise
+ */
+
+int _isalpha(int c)
+{
+	int letter = 0;
+	char lower, upper;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (lower == c || upper == 'C')
+			{
+				letter = 1;
+			}
+		}
+	}
+	return (letter);
+
+}

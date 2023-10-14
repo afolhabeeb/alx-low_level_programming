2a4978338a500eed5bb9c3b3922980a3be2ed5f0
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-10-13 19:45:50 -0700

    Numbers constitute

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,4 @@
-#include "mian.h"
+#include "main.h"
 
 /**
  * more_numbers - print 0 to 14 10 times
@@ -9,16 +9,16 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = '0'; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j >= '10')
+			if (j >= 10)
 			{
-				_putchar(j / '10');
+				_putchar((j / 10) + '0');
 			}
-			_putchar(j % '10');
+			_putchar((j % 10) + '0');
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }

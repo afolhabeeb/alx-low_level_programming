e7f695cf14d061e3084a0d359bda72c490f851dc
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-10-13 20:18:29 -0700

    sqaure

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - print sqaure
+ * @size: no of square
+ *
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+

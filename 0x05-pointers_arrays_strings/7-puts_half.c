c7231843b20b740cc73a3a8e1eaa2e3cc7fc1761
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-10-16 20:56:19 -0700

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of string
+ * @str: indicate string
+ *
+ * Return; void
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int count, n;
+
+	count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		count++;
+	}
+	n = (count - 1) / 2;
+
+	for (i = n + 1; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}

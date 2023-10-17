f0c4e4c4d0a13933629aae792cbfdaf0aecf2cf4
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-10-16 20:16:59 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *  rev_string - reverses a string
+ *  @s: indicate string
+ *
+ *  Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	char c;
+	int count;
+
+	count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	for (i = 0; i <= count / 2; i++)
+	{
+		c = s[i];
+		s[i] = s[count - 1 - i];
+		s[count - 1 - i] = c;
+
+	}
+}

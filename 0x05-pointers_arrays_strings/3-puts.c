8ce33425cae0078d24f28ebb28300da5d38e1531
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-10-16 19:35:21 -0700

    standard output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a strng
+ * @str: indicate the string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+
+	}
+	_putchar('\n');
+
+}

9297dcc1e3d48fd7292b61892eeaf6c5ae6df3fd
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-10-24 21:55:20 -0700

    Value pointer string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of a pointer to a character
+ *
+ * @s: character one
+ * @to: string to source
+ *
+ * Return: print
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

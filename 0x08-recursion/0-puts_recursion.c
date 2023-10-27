0fca2adf623f34df71514e4636aecd562e13059e
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-10-26 18:25:11 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string followed by a new line
+ *
+ * @s: string
+ *
+ * Return: always zero
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

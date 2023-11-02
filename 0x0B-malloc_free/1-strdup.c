2c7d168d8ebfea3801f4512cf049abe2c590c44a
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-11-01 18:47:40 -0700

    Copy string to newly allocated string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * _strdup - returns a pointer to a new string
+ * which is a duplicate of the string str
+ *
+ * @str: memory allocation address pointer
+ *
+ * Return: return void
+ */
+
+char *_strdup(char *str)
+{
+	size_t i, j;
+	char *stralt;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++
+	}
+	stralt = malloc(sizeof(char) * (i + 1));
+
+	if (stralt == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+	{
+		stralt[j] = str[j];
+		return (stralt);
+	}
+}

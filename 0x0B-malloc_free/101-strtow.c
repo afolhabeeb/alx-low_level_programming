d0c9f33f8e63439fd9a17399d6e365f5e08aae17
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-11-05 02:09:09 -0800

    do over

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
-#include <stddef.h>
 
 /**
  * ch_free_grid - main etry
@@ -12,13 +10,11 @@
 
 void ch_free_grid(char **grid, size_t height)
 {
-	if (grid != NULL || height != 0)
+	if (grid != NULL && height != 0)
 	{
 		for (; height > 0; height--)
-		{
+		free(grid[height]);
 			free(grid[height]);
-			free(grid);
-		}
 		free(grid);
 	}
 
@@ -33,51 +29,43 @@ void ch_free_grid(char **grid, size_t height)
 
 char **strtow(char *str)
 {
-	size_t i, j, c, height, al;
 	char **aout;
+	size_t c, height, i, j, a1;
 
 	if (str == NULL || *str == '\0')
-	{
 		return (NULL);
-	}
 	for (c = height = 0; str[c] != '\0'; c++)
-	{
 		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-		{
-			height++;
-		}
-		aout = malloc(sizeof(char *) * (height + 1));
-		if (aout == NULL || height == 0)
+		height++;
+	{
+		aout = malloc((height + 1) * sizeof(char *));
+	}
+		if (aout == NULL)
 		{
 			free(aout);
 			return (NULL);
 		}
-	}
-
-	for (i = al = 0; i < height; i++)
+	for (c = a1 = 0; i < height; i++)
 	{
-		for (c = al; str[c] != '\0'; c++)
+		for (c = a1; str[c] != '\0'; c++)
 		{
 			if (str[c] == ' ')
-			{
-				al++;
-			}
+				a1++;
 			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
-				aout[i] = malloc((c - al + 2) * sizeof(char));
+				aout[i] = malloc((c - a1 + 2) * sizeof(char));
 				if (aout[i] == NULL)
 				{
-					ch_free_grid(aout, i);
-					return (NULL);
+				ch_free_grid(aout, i);
+				return (NULL);
 				}
 				break;
 			}
 		}
-		for (j = 0; al <= c; al++, j++)
-			aout[i][j] = str[al];
+		for (j = 0; a1 <= c; j++, a1++)
+			aout[i][j] = str[a1];
 		aout[i][j] = '\0';
 	}
 	aout[i] = NULL;
 	return (aout);
-
 }

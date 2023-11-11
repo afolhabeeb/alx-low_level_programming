b604e349d67922cc1deb28f2f6499c5a3ff743b5
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-11-11 14:47:09 -0800

    up to date

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,10 +20,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		wrd = va_arg(str, char*);
+		wrd = va_arg(str, char *);
 
 		if (str == NULL)
+		{
 			printf("(nil)");
+		}
 		else
 		{
 			printf("%s", wrd);
@@ -35,7 +37,5 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 	printf("\n");
 	va_end(str);
-
-
 }
 

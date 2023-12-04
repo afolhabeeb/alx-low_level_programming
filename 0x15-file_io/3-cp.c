d580d691103554bbb156812e81f3b7e206fe98c1
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-12-03 16:31:34 -0800

    corrected

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,7 +21,7 @@ void err_file(int file_from, int file_to, char *argv[])
 
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
@@ -38,35 +38,36 @@ int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
 	ssize_t nch, nwr;
-	char buff[1024];
+	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to \n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND);
-
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	err_file(file_from, file_to, argv);
-	nch = 1024;
 
+	nch = 1024;
 	while (nch == 1024)
 	{
-		nch = read(file_from, buff, 1024);
+		nch = read(file_from, buf, 1024);
 		if (nch == -1)
 			err_file(-1, 0, argv);
-
-		nwr = write(file_to, buff, nch);
-		if (nwr == -1)
+		nwr = write(file_to, buf, nch);
+			if (nwr == -1)
 			err_file(0, -1, argv);
 	}
+
 	err_close = close(file_from);
 	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't clode fd %d\n", file_from);
 		exit(100);
 	}
+
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
